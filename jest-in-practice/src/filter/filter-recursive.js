c4df5991da0ef99a1edc1e4f8bc9512cb9f3566a
Rafael Moura <rafaelmoura2929@gmail.com>
2020-04-19 14:09:49 -0300

    Fazendo filter funcional

--- jest-in-practice/src/filter/filter-recursive.js
+++ jest-in-practice/src/filter/filter-recursive.js
@@ -1,14 +1,12 @@
 'use strict'
 
 const filter = (arr, func) => {
-    let newArr = []
-
-    for (let i = 0; i < arr.length; i++) {
-        if(func(arr[i], i, arr)){
-            newArr.push(arr[i])
-        }
-    }
-    return newArr
+    return(function filterInternal(arrayInternal, counter){
+        const [head, ...tail] = arrayInternal
+        return arrayInternal.length === 0 
+        ? []
+        : (func(head, counter, arr) ? [head] : []).concat(filterInternal(tail, counter + 1))
+    })(arr, 0)
 }
 
 export default filter
